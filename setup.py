41a802767f1c11999aa71ad8bc10272080c07a13
Andrew Lighten <andrew@digital-ironworks.com>
2019-11-26 15:04:25 +1100

    Updates to setup.py
    - Added installation dependencies
    - Added further classifiers

--- setup.py
+++ setup.py
@@ -11,10 +11,26 @@ setup(
     description="Contact manager",
     long_description="A tool for searching and displaying contact details based on command line queries",
     keywords="contacts",
-    classifiers=["Development Status :: 4 - Beta", "License :: OSI Approved :: Apache Software License", "Intended Audience :: End Users/Desktop"],
+    classifiers=[
+        "Environment :: Console",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: End Users/Desktop",
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
+        "Topic :: Communications :: Email",
+        "Topic :: Office/Business",
+    ],
     entry_points={"console_scripts": ["contacts = contacts:main",],},
     python_requires=">=3.7.5",
     license="https://www.apache.org/licenses/LICENSE-2.0",
     url="https://github.com/AndrewLighten/contacts",
-    platforms=["Operating System :: MacOS :: MacOS X", "Operating System :: POSIX :: Linux"]
+    platforms=[
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        "Click>=7.0",
+        "termcolor>=1.1.0"
+    ]
 )
